020bba002404e863c101a552aebee2f8ef032103
= <=>
2015-04-21 12:57:21 +0100

    * removed unused files

--- employeedemo/ContactKind.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace EFDemo {
-    public enum ContactKind {
-        Phone,
-        Email
-    }
-}
--- employeedemo/IEmployeeRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace EFDemo {
-    public interface IEmployeeRepository {
-        void Save(Employee employee);
-        Employee Load(int employeeId);
-    }
-}
